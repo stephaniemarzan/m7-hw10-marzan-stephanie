86f04a292c7bbdf667edf31fd7a7d187275c7a7c
Stephanie Marzan <s.marzanoberholz@ufl.edu>
2022-03-25 13:40:45 -0500

    added starter files

---
+++ index.js
@@ -0,0 +1,37 @@
+// acquire references to page elements
+var nameSpan = document.querySelector('span')
+var formEl = document.querySelector('form')
+var clear = document.querySelector('#clear')
+var textarea = document.querySelector('textarea')
+
+// Retrieve name and note content from cookies and localstorage
+// Then apply them to elements on the page
+// YOUR CODE HERE
+
+formEl.onsubmit = function(e) {
+  // prevents form submission
+  e.preventDefault()
+  // save name element's content to cookies
+  // save textarea's content to localstorage
+  // YOUR CODE HERE
+
+  // triggers thumbs up animation
+  this.elements.save.classList.add('emoji')
+}
+
+clear.onclick = function() {
+  // Clear textarea's value
+  // Clear localstorage's content
+  // YOUR CODE HERE
+
+  // triggers thumbs up animation
+  this.classList.add('emoji')
+}
+
+// this code allows repeated thumbs up animations
+function endThumbsUp() {
+  this.classList.remove('emoji')
+}
+
+formEl.elements.save.onanimationend = endThumbsUp
+clear.onanimationend = endThumbsUp
